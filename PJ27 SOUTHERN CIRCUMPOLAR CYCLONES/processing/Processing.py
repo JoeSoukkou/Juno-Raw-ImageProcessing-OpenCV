f96c98cb5e57e0466de1eaa5e8641be6ca01ca98
Youcef Soukkou <youcefsoko66@gmail.com>
2020-06-24 19:01:24 +0100

    created a disparity map Using JUNO PJ27 SOUTHERN CIRCUMPOLAR CYCLONES IMAGE SET

--- PJ27 SOUTHERN CIRCUMPOLAR CYCLONES/processing/Processing.py
+++ PJ27 SOUTHERN CIRCUMPOLAR CYCLONES/processing/Processing.py
@@ -1,1 +1,11 @@
 # JUNO PJ27 SOUTHERN CIRCUMPOLAR CYCLONES IMAGE SET PROCESSING OPENCV
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+imgL = cv.imread('ImageSet/JNCE_2020154_27C00059_V01-mapprojected.png',0)
+imgR = cv.imread('ImageSet/JNCE_2020154_27C00059_V01-red.png',0)
+stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL,imgR)
+plt.imshow(disparity,'gray')
+plt.show()
